68a989fdc0967cc0ab7c7287c2b01529a90a17c1
Abdullah Talayhan <abdullahttj@gmail.com>
2022-08-01 01:22:37 +0300

    append redirect

--- assets/javascripts/discourse/initializers/custom_redirect.js
+++
@@ -1,6 +0,0 @@
-export default {
-    name: 'custom_redirect',
-    initialize() {
-      console.log("pathname", window.location.pathname);
-    }
-  };
--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,8 @@
 # about: Redirect to a custom route
 # version: 0.1
 # authors: Abdullah Talayhan
+relative_url_root = (defined?(Rails.configuration.relative_url_root) && Rails.configuration.relative_url_root) ? Rails.configuration.relative_url_root + '/' : '/'
+
+Discourse::Application.routes.append do
+    get '/:year/:id', to: redirect(relative_url_root + '%{year}/%{name}') , constraints: {year: /[0-9+]/, id: year: /[0-9+]/}
+end
