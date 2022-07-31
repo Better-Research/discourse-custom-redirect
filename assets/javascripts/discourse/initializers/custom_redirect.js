67136956e963697ed53181aa06ad968b3e2c8580
Abdullah Talayhan <abdullahttj@gmail.com>
2022-08-01 00:54:58 +0300

    log pathname to console.

---
+++ assets/javascripts/discourse/initializers/custom_redirect.js
@@ -0,0 +1,6 @@
+export default {
+    name: 'custom_redirect',
+    initialize() {
+      console.log("pathname", window.location.pathname);
+    }
+  };
---
+++ plugin.rb
@@ -0,0 +1,4 @@
+# name: custom-redirect
+# about: Redirect to a custom route
+# version: 0.1
+# authors: Abdullah Talayhan
